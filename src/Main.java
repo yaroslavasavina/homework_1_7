7f32bb823f8c67b8f8534af404541ccf75235123
yaroslavasavina <savina.yaroslava.igorevna@gmail.com>
2022-07-27 17:37:23 +0300

    task 1+2+3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Task 1.
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        // Task 2.
+        String fullNameBig = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameBig);
+
+        // Task 3.
+        String firstName2 = "Семён";
+        String middleName2 = "Семёнович";
+        String lastName2 = "Иванов";
+        String fullName2 = lastName2 + " " + firstName2 + " " + middleName2;
+        fullName2 = fullName2.replace("ё", "е");
+        System.out.println(fullName2);
+
     }
 }
